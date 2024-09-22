7f840be6571d74666ccefada5a6baeeac1b5eb47
Daniela Mansilla <danielamansill29@gmail.com>
2024-09-22 00:41:51 -0300

    preentrega

---
+++ tours.js
@@ -0,0 +1,52 @@
+alert ("¡Bienvenido!");
+let nombre = prompt("¿Cual es tu nombre?").toUpperCase();
+
+
+while (nombre.length <2) {
+    nombre = prompt ("El nombre debe de contener más de una letra, ingrese nuevamente, gracias!").toUpperCase()
+ }
+
+ let edad = prompt("¿Cual es tu edad?");
+
+
+document.querySelector('span');
+
+document.getElementsByTagName('i')
+
+welcome.textContent = "Hola, " + nombre;
+
+const i = document.querySelector("i");
+
+i.setAttribute("class", "fa fa-ticket");
+
+document.querySelector('span');
+
+welcome.innerHTML = '<i class="fa-solid fa-ticket"></i>' + " " + "Hola, " + nombre + " ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
